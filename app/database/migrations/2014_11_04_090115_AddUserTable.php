505899b2a586e21cc070abddf12e89233575f9dc
Mark Spruijt <me@nightduty.nl>
2014-11-04 10:13:14 +0100

    Database migrations added
    Added some migrations.
    AddUserTable done.
    Added database erd.

---
+++ app/database/migrations/2014_11_04_090115_AddUserTable.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema:create('users', function($table)){
+			$table->increments('id');
+			$table->string('email', 255);
+			$table->string('password', 1024);
+			$table->boolean('admin')->default(false);
+			$table->boolean('blocked')->default(false);
+			$table->string('key', 255);
+		}
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema:drop('users');
+	}
+
+}
