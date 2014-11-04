73e69bbd364902fa81ac2991d7ffe7a1cce7162c
Sebasdc <sebastiaandeconinck10@hotmail.com>
2014-11-04 13:25:37 +0100

    Fixed style sheet link

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -1,6 +1,6 @@
 <!doctype html>
 	<head>
-		<link rel="stylesheet" type="text/css" href="{{URL::asset('css/style.css')}}">
+		<link rel="stylesheet" type="text/css" href="{{URL::asset('css/css.css')}}">
 	</head>
 	<body>
 		@yield('content')
