7d4ff2a7183e0ace58d313897a1d8daf1aeffc5e
Sebasdc <sebastiaandeconinck10@hotmail.com>
2014-11-04 17:17:26 +0100

    Added Account Editting

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -27,18 +27,43 @@ class UserController extends BaseController {
 	public function edit()
 	{
 		$input = Input::all();
-		dd($input);
+		$user = Auth::User();
+		$errors = [];
 
-		return View::make('account.edit');
+		if(isset($input['new_password'][0][0]) && isset($input['new_password'][1][0]) && isset($input['old_password'][0])){
+			if(isset($input['new_password'][0][8]) && isset($input['new_password'][1][8])){
+				if($input['new_password'][0] == $input['new_password'][1]){
+					if(Auth::validate(array('email' => $user->email, 'password' => $input['old_password']))){
+						$user->password = Hash::make($input['new_password'][0]);
+						$user->save();
+					}
+					else{
+						$errors[] = "Het oude wachtwoord is niet onjuist.";
+					}
+				}
+				else{
+					$errors[] = "Het nieuwe wachtwoord komt niet overeen met het herhalingswachtwoord.";
+				}
+			}
+			else{
+				$errors[] = "Het nieuwe wachtwoord moet minimaal 8 karakters bevatten.";
+			}
+		}
+		else{
+			$errors[] = "Er zijn een of meerdere velden niet ingevuld.";
+		}
+		return View::make('account.edit')->withErrors($errors);
 	}
+
 
 	public function showNew()
 	{
 		return View::make('account.new');
 	}
 
-	public function new()
+	public function newn()
 	{
 
 	}
+
 }
--- app/views/account/edit.blade.php
+++ app/views/account/edit.blade.php
@@ -6,9 +6,11 @@ LunchTime - Login
 
 @section('content')
 <h1>Wachtwoord Wijzigen</h1>
-@if(isset($error))
-	<label>{{$error}}</label>
-@endif
+
+@foreach($errors->all() as $error)
+	<label>Error: {{$error}}</label><br/>
+@endforeach
+
 {{Form::open()}}
 
 {{Form::label('old_password','Oud wachtwoord:')}}
@@ -17,7 +19,7 @@ LunchTime - Login
 {{Form::label('new_password[0]','Nieuw wachtwoord:')}}
 {{Form::password('new_password[0]')}}
 
-{{Form::label('new_password[1]','Herhaling nieuw wachtwoord:')}}
+{{Form::label('new_password[1]','Herhaal wachtwoord:')}}
 {{Form::password('new_password[1]')}}
 
 {{Form::submit('Wijzigen')}}
