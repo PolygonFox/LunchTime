e085f09c14deec1f48750a4d01ff429407c81479
Sebasdc <sebastiaandeconinck10@hotmail.com>
2014-11-10 10:29:40 +0100

    Added protection for routes

--- app/routes.php
+++ app/routes.php
@@ -6,12 +6,20 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
-Route::get('/new', array('as' => 'new', 'uses' => 'ShoppinglistController@getNew'));
-Route::post('/new', array('uses' => 'ShoppinglistController@postNew'))->before('csrf');
 Route::get('/login', 'UserController@showLogin');
 Route::post('/login', 'UserController@login');
-Route::get('/logout', 'UserController@logout');
-Route::get('/account/edit', 'UserController@showEdit');
-Route::post('/account/edit', 'UserController@edit');
-Route::get('/account/new', 'UserController@showNew');
-Route::post('/account/new', 'UserController@new');
+
+/* Protected */
+Route::group(array('before' => 'auth'), function()
+{
+	/* Account */
+	Route::get('/account/edit', 'UserController@showEdit');
+	Route::post('/account/edit', 'UserController@edit');
+	Route::get('/account/new', 'UserController@showNew');
+	Route::post('/account/new', 'UserController@new');
+	Route::get('/logout', 'UserController@logout');
+
+	/*	Shopping List */
+	Route::get('/new', 'ShoppinglistController@getNew');
+	Route::post('/new', 'ShoppinglistController@postNew');
+});
