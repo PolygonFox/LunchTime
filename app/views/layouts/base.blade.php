184326559564e5c7f3b5dcb78ea6b5ef33772830
Sebasdc <sebastiaandeconinck10@hotmail.com>
2014-11-10 14:21:46 +0100

    Placed messages underneath the header.

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('vulling')
-		{{{ isset($message ) ? $message : '' }}}
+		
 		<a href="{{URL::to('')}}">Overzicht</a>
 		<a href="{{URL::to('account')}}">Mijn Account</a>
 		@if(Auth::User()->admin)
@@ -8,6 +8,7 @@
 		@endif
 		<a href="{{URL::to('logout')}}">Uitloggen</a>
 		<div class='content'>
+			{{{ isset($message ) ? $message : '' }}}
 			@yield('content')
 		</div>
 @stop
