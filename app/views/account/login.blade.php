80f6e799864b4fe477e1e4b92ced4d585fac8cc0
Mark Spruijt <me@nightduty.nl>
2014-11-10 12:26:41 +0100

    Error pages added,
    Title fixed

--- app/views/account/login.blade.php
+++ app/views/account/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.base')
 
 @section('title')
-LunchTime - Login
+Login
 @stop
 
 @section('content')
---
+++ app/views/error/403.blade.php
@@ -0,0 +1,9 @@
+@extends('base')
+
+@section('title')
+Error 404
+@stop
+
+@section('content'){
+	<h1>Sorry, This page knows alot of secret details about the internet and we cant let you see those files. Forbidden 403</h1><hr>
+}
