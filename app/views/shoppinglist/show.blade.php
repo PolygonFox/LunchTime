34fe8a68bc572855f7cdf5da184408890a5acb7a
Mark Spruijt <me@nightduty.nl>
2014-11-11 09:20:22 +0100

    Toggle Locked now works in template aswell

--- app/views/shoppinglist/show.blade.php
+++ app/views/shoppinglist/show.blade.php
@@ -6,7 +6,13 @@ Boodschappenlijst
 
 @section('content')
 <h1>Boodschappenlijst van {{ date('d M Y',strtotime($shoppinglist->created_at)) }}</h1>
-<a href="{{URL::to("boodschappenlijst/lock/{$shoppinglist->id}")}}">Toggle Lock</a>
+<a href="{{URL::to("boodschappenlijst/lock/{$shoppinglist->id}")}}">
+	@if($shoppinglist->locked == 0)
+		Vergrendel
+	@else
+		Ontgrendel
+	@endif
+</a>
 <ul>
 	@foreach($shoppinglist->item as $item)
 	<li>
