bb38e1ecfcd5435e49087e6d991db92099cb3453
sandrr26 <sandersteehouwer@hotmail.com>
2014-11-11 11:09:08 +0100

    Added Bootstrap Template

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -1,11 +1,28 @@
 <!doctype html>
 	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="{{URL::asset('css/css.css')}}">
+		<link rel="stylesheet" type="text/css" href="{{URL::asset('css/simple-sidebar.css')}}" >
+		<link rel="stylesheet" type="text/css" href="{{URL::asset('css/bootstrap.min.css')}}" >
+
 		<title>LunchTime - @yield('title')</title>
 	</head>
 	<body>
-		<div class="main_container">
-		@yield('vulling')
-	</div>
+		<div id="wrapper">
+			@yield('vulling')
+		</div>
+	    <script src="{{URL::asset('js/jquery.js')}}"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
 	</body>
 </html>
