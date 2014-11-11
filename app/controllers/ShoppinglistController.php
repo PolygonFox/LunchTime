51f2ab695daeab33d4f4a490752e79a81b453618
Sebasdc <sebastiaandeconinck10@hotmail.com>
2014-11-11 13:05:31 +0100

    ShoppinglistController fixed after random merge fail.

--- app/controllers/ShoppinglistController.php
+++ app/controllers/ShoppinglistController.php
@@ -3,18 +3,24 @@
 class ShoppinglistController extends BaseController {
 
 public function show($id){
+
 	$shoppinglist = Shoppinglist::find($id);
-	$shoppinglist->user = User::where('user_id');
 	return View::make('Shoppinglist.show')->withShoppinglist($shoppinglist);
 }
 
+public function delete($lijst_id, $item_id)
+{
+
+	$shoppinglist = Shoppinglist::destroy($item_id);
+	return Redirect::intended("/boodschappenlijst/{$lijst_id}");
 }
 
-
-
+public function getNew()
+{
+	$shoppinglists = Shoppinglist::all();
 	return View::make('new')->withShoppinglists($shoppinglists);
-
 }
+
 public function postNew(){
 
 
@@ -22,9 +28,9 @@ public function postNew(){
 	$shoppinglist->user_id = Auth::User()->id;
 	$shoppinglist->save();
 
-	
 	return Redirect::intended('/boodschappenlijsten');
 }
+
 public function lock($id){
 	$list = Shoppinglist::find($id);
 	if($list->locked == 1){ $list->locked = 0; }
