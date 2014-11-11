d5191937097d3c22e16529d458fb194c29774f7d
Sebasdc <sebastiaandeconinck10@hotmail.com>
2014-11-11 10:48:09 +0100

    Fix because I fail at merging ;)

--- app/controllers/ShoppinglistController.php
+++ app/controllers/ShoppinglistController.php
@@ -28,16 +28,8 @@ public function postNew(){
 
 	
 	return Redirect::intended('/boodschappenlijsten');
-	}
-
-	public function lock($id){
-		$list = Shoppinglist::find($id);
-		if($list->locked == 1){ $list->locked = 0; }
-		else{ $list->locked = 1; }
-		$list->save();
-		return Redirect::to('boodschappenlijst/'. $id);
-	}
 }
+
 public function lock($id){
 	$list = Shoppinglist::find($id);
 	if($list->locked == 1){ $list->locked = 0; }
