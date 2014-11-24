cfe119c5fdeeb5911cd8df7565fc226aee236320
Sebasdc <Sebastiaandeconinck10@hotmail.com>
2014-11-24 15:56:13 +0100

    Timestamps of Shoppinglists are now more detailed if there are more Shoppinglists with the same date.

--- app/views/new.blade.php
+++ app/views/new.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.base')
 
 @section('content')
-			<h1>Maak een nieuw boodschappenlijst</h1>
-			
-		
-			
+			<h1>Maak een nieuwe boodschappenlijst</h1>	
 			{{Form::open(array('url' => URL::to('/new')))}}
-				<p><input type="submit" value="Nieuw lijst"/></p>
+				<p><input type="submit" value="Nieuwe lijst"/></p>
 			{{Form::close()}}
 			<ul>
 				@foreach($shoppinglists as $shoppinglist)
-				<li><a href="{{URL::to("boodschappenlijst/{$shoppinglist->id}")}}">Boodschappenlijst van {{ date('d M Y',strtotime($shoppinglist->created_at)) }}</a></li>
+				<li><a href="{{URL::to("boodschappenlijst/{$shoppinglist->id}")}}">Boodschappenlijst van 
+					@if($shoppinglist->detailed)
+						{{ date('d M Y H:i:s',strtotime($shoppinglist->created_at)) }}
+					@else
+						{{ date('d M Y',strtotime($shoppinglist->created_at)) }}
+					@endif
+				</a></li>
 				@endforeach
 			</ul>
 
